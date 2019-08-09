c589013e07c8c07662c8079ab232b411bbf33195
Rodolfo <brsrkdev@gmail.com>
2019-08-09 14:30:14 -0500

    Add my_each_with_index method

--- main.rb
+++ main.rb
@@ -7,5 +7,17 @@ module Enumerable
     end
     self
   end 
+
+  def my_each_with_index
+    i = 0
+    while i < self.size
+      yield(self[i], i)
+      i += 1
+      self
+    end
+  end
 end
 
+[:foo, :bar, :baz].my_each_with_index(2) do |value, index|
+  puts "#{index}: #{value}"
+end
