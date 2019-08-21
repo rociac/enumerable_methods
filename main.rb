48724294d1bc690c99ab1f9a5a83df132b4dfe39
Rodolfo Cisneros <brsrkdev@gmail.com>
2019-08-21 12:56:45 -0500

    Fix my_map method and linter errors

--- main.rb
+++ main.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
     i = 0
-    while i < self.size
+    while i < size
       yield (self[i])
       i += 1
     end
     self
-  end 
+  end
 
   def my_each_with_index
     i = 0
-    while i < self.size
+    while i < size
       yield(self[i], i)
       i += 1
       self
@@ -19,23 +21,23 @@ module Enumerable
 
   def my_select
     result = []
-    self.my_each {|item| result.push(item) if yield(item)}
+    my_each { |item| result.push(item) if yield(item) }
     result
   end
 
   def my_all?
-    self.my_each {|item| eturnr false if yield(item) == false}
+    my_each { |item| eturnr false if yield(item) == false }
     true
   end
 
   def my_any?
-    self.my_each {|item| return true if yield(item)}
+    my_each { |item| return true if yield(item) }
     false
   end
 
   def my_none?
     result = true
-    self.my_each do |item| 
+    my_each do |item|
       if yield(item)
         result = false
         break
@@ -47,44 +49,39 @@ module Enumerable
   def my_count
     count = 0
     if block_given?
-      self.my_each {|item| count += 1 if yield(item)}
+      my_each { |item| count += 1 if yield(item) }
     else
-      count = self.size
+      count = size
     end
     count
   end
 
-  def my_map(&proc)
-    return self.to_enum unless block_given?
-		new_array = []
-		if self.class == Hash
-			self.each do |k, v|
-				new_array << proc.call(k, v)
-			end
-			new_array
-		else
-			self.my_each do |x|
-				new_array << proc.call(x)
-			end
-			new_array
-		end
+  def my_map(proc = nil)
+    new_array = []
+    if proc.nil?
+      my_each { |i| new_array << yield(i) }
+    elsif !proc.nil? && block_given?
+      my_each { |i| new_array << proc.call(i) }
+    else
+      my_each { |i| new_array << proc.call(i) }
+    end
+    new_array
   end
 
   def my_inject
     i = 1
     sum = self[0]
-    while i < self.size
+    while i < size
       sum = yield(sum, self[i])
       i += 1
-      end
-    return sum
+    end
+    sum
   end
 end
 
 def multiply_els(array)
-  array.my_inject {|product, item| product * item }
+  array.my_inject { |product, item| product * item }
 end
 
 ary = [1, 2, 4, 2]
 puts multiply_els(ary)
-
